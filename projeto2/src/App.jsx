33cc2165a48ddf5a7341980f64f415edebe3b78d
MarcosBizzarri <marcos-bizzarri@hotmail.com>
2023-06-28 19:08:47 -0300

    Initial commit

---
+++ projeto2/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home" ;
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      
+    </div>
+  )
+} 
+
+export default App
